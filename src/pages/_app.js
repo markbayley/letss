8ba7e85dbfd0fd826483f5ce5a8001c43248e7c8
Mark <markyb152@hotmail.com>
2022-07-04 01:16:12 +1000

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Navbar from "./../components/Navbar";
+import IndexPage from "./index";
+import AboutPage from "./about";
+import FaqPage from "./faq";
+import ContactPage from "./contact";
+import PricingPage from "./pricing";
+import DashboardPage from "./dashboard";
+import AuthPage from "./auth";
+import SettingsPage from "./settings";
+import LegalPage from "./legal";
+import { Switch, Route, Router } from "./../util/router";
+import PurchasePage from "./purchase";
+import FirebaseActionPage from "./firebase-action";
+import NotFoundPage from "./404";
+import Footer from "./../components/Footer";
+import "./../util/analytics";
+import { AuthProvider } from "./../util/auth";
+import { ThemeProvider } from "./../util/theme";
+import { QueryClientProvider } from "./../util/db";
+
+function App(props) {
+  return (
+    <QueryClientProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <Router>
+            <>
+              <Navbar
+                color="default"
+                logo="/letss3.png"
+                logoInverted="/letssdark.png"
+              />
+
+              <Switch>
+                <Route exact path="/" component={IndexPage} />
+
+                <Route exact path="/about" component={AboutPage} />
+
+                <Route exact path="/faq" component={FaqPage} />
+
+                <Route exact path="/contact" component={ContactPage} />
+
+                <Route exact path="/pricing" component={PricingPage} />
+
+                <Route exact path="/dashboard" component={DashboardPage} />
+
+                <Route exact path="/auth/:type" component={AuthPage} />
+
+                <Route
+                  exact
+                  path="/settings/:section"
+                  component={SettingsPage}
+                />
+
+                <Route exact path="/legal/:section" component={LegalPage} />
+
+                <Route exact path="/purchase/:plan" component={PurchasePage} />
+
+                <Route
+                  exact
+                  path="/firebase-action"
+                  component={FirebaseActionPage}
+                />
+
+                <Route component={NotFoundPage} />
+              </Switch>
+
+              <Footer
+                bgColor="default"
+                size="medium"
+                bgImage=""
+                bgImageOpacity={1}
+                description="Online learning made easy"
+                copyright={`© ${new Date().getFullYear()} Company`}
+                logo="/letss3.png"
+                logoInverted="letssdark.png"
+                sticky={true}
+              />
+            </>
+          </Router>
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Meta from "./../components/Meta";
+import HeroSection from "./../components/HeroSection";
+import FeaturesSection from "./../components/FeaturesSection";
+import ClientsSection from "./../components/ClientsSection";
+import TestimonialsSection from "./../components/TestimonialsSection";
+import NewsletterSection from "./../components/NewsletterSection";
+import CtaSection from "./../components/CtaSection";
+
+function IndexPage(props) {
+  return (
+    <>
+      <Meta />
+      <HeroSection
+        bgColor="primary"
+        size="large"
+        bgImage="https://cdn.dribbble.com/users/1217289/screenshots/15069381/media/cacd8cf8bb6ad7c6576cd8e525c2f233.jpg"
+        bgImageOpacity={0.7}
+        title="Welcome to LETSS!"
+        subtitle="LEARNING ENGLISH MADE EASY."
+        buttonText="Get Started"
+        buttonColor="secondary"
+        buttonPath="/pricing"
+      />
+      <FeaturesSection
+        bgColor="default"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Why Learn With Us?"
+        subtitle="We offer a unique, fun, conversational approach to learning English"
+      />
+      <ClientsSection
+        bgColor="light"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="About Dr. Ofi Muzar"
+        subtitle="Learn more about how LETSS began and our founder here..."
+      />
+      <TestimonialsSection
+        bgColor="default"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Here's what people are saying"
+        subtitle=""
+      />
+      <NewsletterSection
+        bgColor="light"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Stay in the know"
+        subtitle="Receive our latest articles and feature updates"
+        buttonText="Subscribe"
+        buttonColor="primary"
+        inputPlaceholder="Enter your email"
+        subscribedMessage="You are now subscribed!"
+      />
+      <CtaSection
+        bgColor="primary"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Ready to get started?"
+        subtitle=""
+        buttonText="Get Started"
+        buttonColor="secondary"
+        buttonPath="/pricing"
+      />
+    </>
+  );
+}
+
+export default IndexPage;
